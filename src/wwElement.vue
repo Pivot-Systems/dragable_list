0a71c77919ea7632d74c6a17a8531d3ba24d9237
Aurélie VIOLETTE <orwel.violette@gmail.com>
2023-06-29 15:19:18 +0200

    Init project

---
+++ src/wwElement.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul class="pf-list"></ul>
+</template>
+
+<script>
+export default {
+    props: {
+        content: { type: Object, required: true },
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
