d1160e7396efb4f21f875b3249db5cabec968038
samZoro <sam@pivotsystems.com.au>
2025-04-03 17:04:04 +1100

    no auto changes

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,13 +1,13 @@
 <template>
     <draggable
-        v-model="items"
+        :list="items"
         :itemKey="getItemKey"
         :disabled="isEditing"
         :animation="200"
         :style="{ ...$attrs.style, ...layoutStyle }"
         v-bind="options"
         @change="onDragChange"
-        :data-group-id="content.group" 
+        :data-group-id="content.group"
     >
         <template #item="{ element, index }">
             <div>
@@ -33,13 +33,8 @@ export default {
         return { layoutStyle: wwLib.useLayoutStyle() };
     },
     computed: {
-        items: {
-            get() {
-                return wwLib.wwCollection.getCollectionData(this.content.data) || [];
-            },
-            set(value) {
-                this.$emit("trigger-event", { name: "update:list", event: { value } });
-            },
+        items() {
+            return wwLib.wwCollection.getCollectionData(this.content.data) || [];
         },
         isEditing() {
             return this.wwEditorState?.isEditing;
@@ -52,7 +47,6 @@ export default {
             if (this.content.group) {
                 options.group = this.content.group;
             }
-
             return options;
         },
     },
@@ -63,29 +57,32 @@ export default {
         /* wwEditor:start */
         getTestEvent() {
             const data = wwLib.wwCollection.getCollectionData(this.content.data);
-            return {
-                value: data,
-            };
+            return { value: data };
         },
         /* wwEditor:end */
         onDragChange(evt) {
-            // If an item was removed (i.e. dragged out of this list)
-            if (evt.removed) {
-                const movedItem = evt.removed.element;
-                const toGroup = evt.to?.dataset?.groupId || null;
-                const toIndex = evt.newIndex;
+            const fromGroup = evt.from?.dataset?.groupId || null;
+            const toGroup = evt.to?.dataset?.groupId || null;
+            const movedItem = evt.added?.element || evt.removed?.element || evt.moved?.element || null;
+            const type = evt.added
+                ? "added"
+                : evt.removed
+                ? "removed"
+                : evt.moved
+                ? "moved"
+                : "unknown";
 
-                this.$emit("trigger-event", {
-                    name: "update:list",
-                    event: {
-                        value: [...this.items], // current items after removal
-                        movedItem,
-                        toGroup,
-                        toIndex,
-                    },
-                });
-            }
-        }
+            this.$emit("trigger-event", {
+                name: "update:list",
+                event: {
+                    type,
+                    movedItem,
+                    fromGroup,
+                    toGroup,
+                    index: evt.added?.newIndex ?? evt.removed?.oldIndex ?? evt.moved?.newIndex ?? null,
+                },
+            });
+        },
     },
 };
 </script>
